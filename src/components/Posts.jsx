a6fca9e4aa543261f3111bf1fde12808fd9b2ed3
yousrik20 <yousri.k.e13@gmail.com>
2025-04-13 16:24:10 +0200

    lesson39

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
@@ -11,28 +11,70 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Box } from "@mui/material";
+import { Box, Checkbox, Menu, MenuItem } from "@mui/material";
+import { Bookmark, BookmarkBorder, Favorite, FavoriteBorder } from "@mui/icons-material";
 
 export default function Posts() {
   const myCards = [
-    { letter: "A", color: "purple", username: "Ali Hassan",imgLink:'https://images.pexels.com/photos/30548780/pexels-photo-30548780/free-photo-of-artist-painting-with-vibrant-colors-in-studio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-    { letter: "N", color: "teal", username: "Alaa Hassan" ,imgLink:'https://images.pexels.com/photos/30989951/pexels-photo-30989951/free-photo-of-lawson-convenience-store-in-snowy-night-japan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
-    { letter: "D", color: "lightblue", username: "Doaa Hassan" ,imgLink:'https://images.pexels.com/photos/28617427/pexels-photo-28617427/free-photo-of-elegant-presentation-of-turkish-tea-in-sunlit-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
-    { letter: "M", color: "royalblue", username: "Walaa Hassan" ,imgLink:'https://images.pexels.com/photos/29107893/pexels-photo-29107893/free-photo-of-traditional-berber-man-in-ksar-of-ait-benhaddou.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+    {
+      letter: "A",
+      color: "purple",
+      username: "Ali Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/30548780/pexels-photo-30548780/free-photo-of-artist-painting-with-vibrant-colors-in-studio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      letter: "N",
+      color: "teal",
+      username: "Alaa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/30989951/pexels-photo-30989951/free-photo-of-lawson-convenience-store-in-snowy-night-japan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      letter: "D",
+      color: "#f09",
+      username: "Doaa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/28617427/pexels-photo-28617427/free-photo-of-elegant-presentation-of-turkish-tea-in-sunlit-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      letter: "M",
+      color: "royalblue",
+      username: "Walaa Hassan",
+      imgLink:
+        "https://images.pexels.com/photos/29107893/pexels-photo-29107893/free-photo-of-traditional-berber-man-in-ksar-of-ait-benhaddou.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
   ];
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const [cardMenu, setcardMenu] = useState(false);
+
+  const renderMenu = (
+    <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+    </Menu>
+  );
+
   return (
-    <Box sx={{ flexGrow: "3"}}>
+    <Box sx={{ flexGrow: "3" }}>
       {myCards.map((item) => {
         return (
           <Card sx={{ maxWidth: 444, mx: "auto", my: 5 }}>
             <CardHeader
               avatar={
-                <Avatar sx={{ bgcolor:item.color }} aria-label="recipe">
+                <Avatar sx={{ bgcolor: item.color }} aria-label="recipe">
                   {item.letter}
                 </Avatar>
               }
               action={
-                <IconButton aria-label="settings">
+                <IconButton onClick={handleClick} aria-label="settings">
                   <MoreVertIcon />
                 </IconButton>
               }
@@ -53,16 +95,24 @@ export default function Posts() {
               </Typography>
             </CardContent>
             <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
+              <Checkbox
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite sx={{ color: "red" }} />}
+              />
               <IconButton aria-label="share">
                 <ShareIcon />
               </IconButton>
+              <Box flexGrow={1}/>
+              <Checkbox
+                
+                icon={<BookmarkBorder />}
+                checkedIcon={<Bookmark />}
+              />
             </CardActions>
           </Card>
         );
       })}
+      {renderMenu}
     </Box>
   );
 }
